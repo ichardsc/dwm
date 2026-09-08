055f0258b4adea73f5e8530383eca8452edf3ee5
ituxcore <ituxcore@gmail.com>
2026-09-08 01:05:33 -0500

    Add a wallpaper and change the autostart settings

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,6 +2,6 @@
 
 xrandr -s 1920x1080 &
 setxkbmap latam &
-feh --bg-fill ~/downloads/tux.jpg &
+feh --bg-fill $HOME/.local/share/wallpapers/default-wallpaper.jpg &
 spice-vdagent &
 ~/.local/bin/bar.sh &
